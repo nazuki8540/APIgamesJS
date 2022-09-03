07b6ba76e54e96961a9d1f48b408b39b50b83fc0
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-09-03 14:12:29 -0300

    update

--- index.js
+++ index.js
@@ -2,78 +2,114 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+
+const JWTSecret = "djkshahjksdajksdhasjkdhasjkdhasjkdhasjkd";
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido!"});
+            }else{
+
+                req.token = token;
+                req.loggedUser = {id: data.id,email: data.email};
+                req.empresa = "Guia do programador";                
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido!"});
+    } 
+}
+
 var DB = {
     games: [
         {
-            id:23,
-            title: "call of duty",
-            year: 2021,
-            price:60
+            id: 23,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
         },
         {
-            id:26,
-            title: "naruto",
-            year: 2001,
-            price:90
+            id: 65,
+            title: "Sea of thieves",
+            year: 2018,
+            price: 40
         },
         {
-            id:28,
-            title: "crash",
+            id: 2,
+            title: "Minecraft",
             year: 2012,
-            price:80
+            price: 20
+        }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Victor Lima",
+            email: "victordevtb@guiadoprogramador.com",
+            password: "nodejs<3"
+        },
+        {
+            id: 20,
+            name: "Guilherme",
+            email: "guigg@gmail.com",
+            password: "java123"
         }
     ]
 }
 
-app.get("/games",(req,res) =>{
+app.get("/games",auth,(req, res) => {
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({empresa: req.empresa,user: req.loggedUser,games: DB.games});
 });
 
-app.get("/game/:id",(req,res) => {
-    
+app.get("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
+        
         var id = parseInt(req.params.id);
 
         var game = DB.games.find(g => g.id == id);
 
         if(game != undefined){
-            res.json(game);
             res.statusCode = 200;
+            res.json(game);
         }else{
             res.sendStatus(404);
         }
-
     }
-
 });
 
-app.post("/game", (req,res) =>{
-
-    var {title,price, year } = req.body;
-
+app.post("/game",auth,(req, res) => { 
+    var {title, price, year} = req.body;
     DB.games.push({
-        id: 232,
+        id: 2323,
         title,
         price,
         year
     });
-
     res.sendStatus(200);
+})
 
-});
-
-app.delete("/game/:id", (req,res) =>{
-
+app.delete("/game/:id",auth,(req, res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
@@ -87,41 +123,78 @@ app.delete("/game/:id", (req,res) =>{
             res.sendStatus(200);
         }
     }
-
 });
 
-app.put("/game/:id", (req,res) => {
+app.put("/game/:id",(req, res) => {
 
     if(isNaN(req.params.id)){
         res.sendStatus(400);
     }else{
+        
         var id = parseInt(req.params.id);
+
         var game = DB.games.find(g => g.id == id);
 
         if(game != undefined){
 
-        var {title,price,year} = req.body;
+            var {title, price, year} = req.body;
 
-        if(title != undefined){
-            game.title = title;
-        }
-        if(price != undefined){
-            game.price = price;
-        }
-        if(year != undefined){
-            game.year = year;
-        }
-        
-        res.sendStatus(200);
+            
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            if(year != undefined){
+                game.year = year;
+            }
+            
+            res.sendStatus(200);
 
         }else{
-           
             res.sendStatus(404);
         }
     }
 
 });
 
-app.listen(4000,()=>{
-    console.log("api rodando")
+
+app.post("/auth",(req, res) => {
+
+    var {email, password} = req.body;
+
+    if(email != undefined){
+
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"});
+            }
+        }else{
+            res.status(404);
+            res.json({err: "O E-mail enviado não existe na base de dados!"});
+        }
+
+    }else{
+        res.status(400);
+        res.send({err: "O E-mail enviado é inválido"});
+    }
+});
+
+app.listen(45679,() => {
+    console.log("API RODANDO!");
 });
